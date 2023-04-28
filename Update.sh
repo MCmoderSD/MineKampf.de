e21a9ff14b8f2c5acfb67d4afb2a4265abbfe296
Seraphin Berger <82591988+MCmoderSD@users.noreply.github.com>
2023-04-28 10:27:38 +0200

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,1 +1,16 @@
-sudo git pull https://github.com/MCmoderSD/MineKampf.de.git ; sudo rm -r /etc/apache2/sites-enabled/minekampf.de.conf ; ln /var/www/minekampf.de/minekampf.de.conf /etc/apache2/sites-enabled/minekampf.de.conf ; sudo systemctl reload apache2
+#!/bin/bash
+
+if [ "$1" = "-auto" ]; then
+  while true; do
+    sudo git pull https://github.com/MCmoderSD/MineKampf.de.git
+    sudo rm -r /etc/apache2/sites-enabled/minekampf.de.conf 
+    ln /var/www/minekampf.de/minekampf.de.conf /etc/apache2/sites-enabled/minekampf.de.conf
+    sudo systemctl reload apache2
+    sleep 30
+  done
+else
+  sudo git pull https://github.com/MCmoderSD/MineKampf.de.git
+  sudo rm -r /etc/apache2/sites-enabled/minekampf.de.conf 
+  ln /var/www/minekampf.de/minekampf.de.conf /etc/apache2/sites-enabled/minekampf.de.conf
+  sudo systemctl reload apache2
+fi
